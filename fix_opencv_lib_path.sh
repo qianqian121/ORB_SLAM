727d08e6657d64326a6574a2a3f058562f328f1e
qianqian121 <qianqian141@hotmail.com>
2017-02-03 13:24:54 -0800

    Fix tegra opencv lib path: added force link
    Fix tegra opencv lib path: added force link

--- fix_opencv_lib_path.sh
+++ fix_opencv_lib_path.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-sudo ln -s /usr/lib/libopencv_videostab.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_videostab.so.2.4.8
-sudo ln -s /usr/lib/libopencv_video.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_video.so.2.4.8
-sudo ln -s /usr/lib/libopencv_superres.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_superres.so.2.4.8
-sudo ln -s /usr/lib/libopencv_stitching.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_stitching.so.2.4.8
-sudo ln -s /usr/lib/libopencv_photo.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_photo.so.2.4.8
-sudo ln -s /usr/lib/libopencv_objdetect.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_objdetect.so.2.4.8
-sudo ln -s /usr/lib/libopencv_ml.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_ml.so.2.4.8
-sudo ln -s /usr/lib/libopencv_legacy.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_legacy.so.2.4.8
-sudo ln -s /usr/lib/libopencv_imgproc.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_imgproc.so.2.4.8
-sudo ln -s /usr/lib/libopencv_highgui.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_highgui.so.2.4.8
-sudo ln -s /usr/lib/libopencv_gpu.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_gpu.so.2.4.8
-sudo ln -s /usr/lib/libopencv_flann.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_flann.so.2.4.8
-sudo ln -s /usr/lib/libopencv_features2d.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_features2d.so.2.4.8
-sudo ln -s /usr/lib/libopencv_core.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_core.so.2.4.8
-sudo ln -s /usr/lib/libopencv_contrib.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_contrib.so.2.4.8
-sudo ln -s /usr/lib/libopencv_calib3d.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_calib3d.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_videostab.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_videostab.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_video.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_video.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_superres.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_superres.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_stitching.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_stitching.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_photo.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_photo.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_objdetect.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_objdetect.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_ml.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_ml.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_legacy.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_legacy.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_imgproc.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_imgproc.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_highgui.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_highgui.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_gpu.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_gpu.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_flann.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_flann.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_features2d.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_features2d.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_core.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_core.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_contrib.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_contrib.so.2.4.8
+sudo ln -s -f /usr/lib/libopencv_calib3d.so.2.4.12 /usr/lib/arm-linux-gnueabihf/libopencv_calib3d.so.2.4.8
